3b3da8dbb106c74a431f63a9d1277606178e5c71
epotapov <43287483+epotapov@users.noreply.github.com>
2021-04-10 19:56:49 -0600

    code for submission

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -205,6 +205,33 @@ def endTimer():
     minute = int(minute % 60)
     print(f"Training Time: {hour} hours {minute} minutes {seconds:0.4f} seconds")
 
+def dataAnalysis():
+    data = pd.read_csv("testoutputwithEvaluations.csv", keep_default_na=False)
+    q1correct = 0
+    q2correct = 0
+    q1num = len(data.axes[0])
+    q2num = len(data.axes[0])
+    for row in data.index:
+        num = row + 1
+        print(f"Reading {num} out of {len(data.axes[0])}")
+        e1 = data.loc[row, "Question 1 Evaluation"]
+        e2 = data.loc[row, "Question 2 Evaluation"]
+        r1 = data.loc[row, "Question 1 Result"]
+        r2 = data.loc[row, "Question 2 Result"]
+
+        if e1 != "N/A":
+            if e1 == r1:
+                q1correct += 1
+        else:
+            q1num -= 1
+
+        if e2 != "N/A":
+            if e2 == r2:
+                q2correct += 1
+        else:
+            q2num -= 1
+    print(f"Question 1: {q1correct}/{q1num} Question 2: {q2correct}/{q2num}")
+
 if __name__ == "__main__":
     if not os.path.isdir("model_artifacts"):
         train, test = load_training_data(limit=20000)
@@ -214,6 +241,7 @@ if __name__ == "__main__":
     print("Testing model")
     ##We still need to work on our neural network before we test the samples collected
     test_csv("OpinionFormEvaluations.csv") 
+    dataAnalysis()
     test_model(Movie_REVIEW)
     test_model(TEST_REVIEW)
     test_model(TEST_REVIEW2)
